bbf5f6fb655bb2c1d010663f1171bc7d36f2a262
dorinus <kiradorin94@gmail.com>
2023-02-13 20:15:05 +0100

    Initial commit

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react"
+
+
+function UserResults() {
+    useEffect(() => {
+     // console.log(process.env.REACT_APP_GITHUB_URL)
+     // console.log(process.env.REACT_APP_GITHUB_TOKEN)
+     // console.log(`${process.env.REACT_APP_GITHUB_URL}/users`)
+        fetchUsers()
+    }, [])
+
+    const fetchUsers = async () => {
+        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`,{
+            headers: {
+                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+                
+            }
+        })        
+
+        const data = await response.json()
+    }
+
+  return (
+    <div>
+      user results
+    </div>
+  )
+}
+
+export default UserResults
