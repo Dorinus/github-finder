dcb7e7102482aaf7c6e8efc6fc674b164fb4c523
dorinus <kiradorin94@gmail.com>
2023-02-15 20:07:19 +0100

    Profile page, refactoring

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom'
 function Navbar({title}) {
   return (
     <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
-        <div className="container max-auto">
+        <div className="container mx-auto">
             <div className="flex-none px-2 mx-2">
                 <FaGithub className='inline pr-2 text-3xl'/>
                 <Link to='/' className='text-lg font-bold align-middle '> {title}</Link>
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,31 +1,28 @@
-import { useEffect } from "react"
+import { useContext } from "react"
+import GithubContext from "../../context/github/GithubContext"
+import Spinner from "../layout/Spinner"
+import UserItem from "./UserItem"
 
 
 function UserResults() {
-    useEffect(() => {
-     // console.log(process.env.REACT_APP_GITHUB_URL)
-     // console.log(process.env.REACT_APP_GITHUB_TOKEN)
-     // console.log(`${process.env.REACT_APP_GITHUB_URL}/users`)
-        fetchUsers()
-    }, [])
+    const {users,isLoading,} = useContext(GithubContext)
 
-    const fetchUsers = async () => {
-        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`,{
-            headers: {
-                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-                
-            }
-        })        
 
-        const data = await response.json()
-        console.log(data)
+
+    
+
+    if(!isLoading){
+      return (
+        <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+          {users.map((user) =>(
+            <UserItem key={user.id} user={user}/>
+          ))}
+        </div>
+      )
+    }else{
+      return <Spinner/>
     }
-
-  return (
-    <div>
-      user results
-    </div>
-  )
+  
 }
 
 export default UserResults
